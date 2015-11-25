4424a6fb8ff506f001ad9efa596d68ba1c24e165
Igor Partola <igor@igorpartola.com>
2015-11-25 16:29:36 -0500

    Fixing version number to be 0.3.

--- src/version.c
+++ src/version.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 const char *program_name   = "hawkeye";
-const char *version_string = "0.2";
+const char *version_string = "0.3";
 const char *author_name    = "Igor Partola";
 const char *email_address  = "igor@igorpartola.com";
-const char *years          = "2013-2014";
-const char *copyright      = "Copyright (C) 2013-2014 Igor Partola\n\
+const char *years          = "2013-2015";
+const char *copyright      = "Copyright (C) 2013-2015 Igor Partola\n\
 This program comes with ABSOLUTELY NO WARRANTY;\n\
 This is free software, and you are welcome to redistribute it\n\
 under terms of the GNU General Public License version 3 or later.";
